7d2acdafceb8b1e9c57f03723c4258bbf8369dea
Tran Thai Duy DOAN <duyconst@gmail.com>
2023-12-25 12:04:33 +0700

    update API

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const API = 'http://localhost:8000/api';
-export const API = 'https://edu-tech-platform-api.onrender.com/api';
+export const API = 'https://edu-tech-platform-api.onrender.com';
