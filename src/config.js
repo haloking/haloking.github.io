35ad064275f7c0c2fa673925dc377b5075325a0a
Tran Thai Duy DOAN <duyconst@gmail.com>
2023-12-25 18:22:15 +0700

    remove port 8000 from API

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const API = 'http://localhost:8000/api';
-export const API = 'https://edu-tech-platform-api.onrender.com:8000/api';
+export const API = 'https://edu-tech-platform-api.onrender.com/api';
