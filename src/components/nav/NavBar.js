598bc94bf4fba59aa06579576bbf7cc9f5d92ab3
Tran Thai Duy DOAN <duyconst@gmail.com>
2024-01-25 09:24:06 +0700

    improve UI- fix deploy-npm i react-scripts@latest-move

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -60,7 +60,7 @@ export default function NavBar() {
                             <div className="offcanvas-body">
                                 <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                                     <li className="nav-item">
-                                        <a className="nav-link mx-lg-2" aria-current="page" href="/">
+                                        <a className="nav-link active mx-lg-2" aria-current="page" href="/">
                                             Trang chủ
                                         </a>
                                     </li>
